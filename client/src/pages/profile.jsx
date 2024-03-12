dbcb71baa77e95f238cf9a0fa0c91b64f7e8ef86
GuillermoM98 <guillermomartinez1998@gmail.com>
2024-03-11 22:37:42 -0500

    message

---
+++ client/src/pages/profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/profile.css';
+
+const ProfilePage = () => {
+    return(
+    <div className = "sample">
+        <h1>Profile Page </h1>
+    </div>
+    );
+}
+
+export default ProfilePage;
