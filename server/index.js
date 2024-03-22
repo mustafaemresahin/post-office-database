da5f7abb94d4a914d5ac0ef0dfa9e73ba00e85de
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-03-21 22:25:02 -0500

    react as static files

--- server/index.js
+++ server/index.js
@@ -61,6 +61,47 @@ const server = http.createServer( async (req, res) => {
   // Handle Cors Function To Allow Axios
   handleCors(req, res);
 
+  // Serve static files from the React app
+  let filePath = path.join(__dirname, 'client/build', req.url === '/' ? 'index.html' : req.url);
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+  };
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      if(error.code == 'ENOENT'){
+        fs.readFile('./404.html', function(error, content) {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      }
+      else {
+        res.writeHead(500);
+        res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+        res.end(); 
+      }
+    }
+    else {
+      res.writeHead(200, { 'Content-Type': mimeTypes[extname] || 'application/octet-stream' });
+      res.end(content, 'utf-8');
+    }
+  });
+
   // GET Requests 
   if (req.method === "GET") {
     if (req.url === "/") {
