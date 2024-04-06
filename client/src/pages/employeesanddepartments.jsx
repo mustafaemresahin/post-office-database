fb6f61e91d67a2142c68ebbba43413d9b9c72929
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-04-06 17:15:00 -0500

    report page

--- client/src/pages/employeesanddepartments.jsx
+++ client/src/pages/employeesanddepartments.jsx
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import '../css/employeesanddepartments.css';
 
 function EmployeesandDepartments() {
   const [data, setData] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchData = async () => {
