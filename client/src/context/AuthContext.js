4a49fa26aea3b255f7989663f07845bdd5a533c0
rmrahman430 <rayyanmrahman@gmail.com>
2024-04-09 18:26:56 -0500

    modified AuthContext

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,32 +1,50 @@
-// AuthContext.js
-import React, { createContext, useContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-const AuthContext = createContext();
+// Create Our Context
+export const AuthContext = createContext();
 
-export const useAuth = () => {
-  return useContext(AuthContext);
-};
+// Local Storage Functions
+const getAuthenticatedId = () => {
+    const authenticatedId = localStorage.getItem("id");
+    return authenticatedId ? JSON.parse(authenticatedId) : "";
+}
 
-export const AuthProvider = ({ children, initialLoggedInState, loginFunction, logoutFunction }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(initialLoggedInState);
+const getUsername = () => {
+    const username = localStorage.getItem("username");
+    return username ? JSON.parse(username) : "";
+}
 
-  const login = () => {
-    if (loginFunction) {
-      loginFunction(); // Call the provided login function
-    }
-    setIsLoggedIn(true);
-  };
+const getCart = () => {
+    const cart = localStorage.getItem("cart");
+    return cart ? JSON.parse(cart) : "";
+}
 
-  const logout = () => {
-    if (logoutFunction) {
-      logoutFunction(); // Call the provided logout function
-    }
-    setIsLoggedIn(false);
-  };
+const getRole = () => {
+    const role = localStorage.getItem("role");
+    return role ? JSON.parse(role) : "";
+}
 
-  return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+
+// Context Function
+export const AuthProvider = ({children}) => {
+    const [currentAuthenticatedId, setCurrentAuthenticatedId] = useState(getAuthenticatedId);
+    const [currentUsername, setCurrentUsername] = useState(getUsername);;
+    const [currentCart, setCurrentCart] = useState(getCart);
+    const [currentRole, setCurrentRole] = useState(getRole);
+
+
+    useEffect(() => {
+        localStorage.setItem("authenticatedId", JSON.stringify(currentAuthenticatedId));
+        localStorage.setItem("username", JSON.stringify(currentUsername));
+        localStorage.setItem("cart", JSON.stringify(currentCart));
+        localStorage.setItem("role", JSON.stringify(currentRole));
+    }, [currentAuthenticatedId, currentUsername, currentCart, currentRole]);
+
+    return (
+        <AuthContext.Provider value={{currentAuthenticatedId, setCurrentAuthenticatedId, currentUsername, 
+        setCurrentUsername, currentCart, setCurrentCart, 
+        currentRole, setCurrentRole}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
