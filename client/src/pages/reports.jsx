dbb8bfb9f1ce98d1f56efa55360f258ae027dd22
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-04-11 13:32:08 -0500

    report page

--- client/src/pages/reports.jsx
+++ client/src/pages/reports.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../css/reports.css'; // Ensure the CSS file path is correct
+import '../css/reports.css';
 
 function Reports() {
   const navigate = useNavigate();
@@ -18,12 +18,13 @@ function Reports() {
       <div className="report-buttons">
         <button onClick={() => goToReport('/salesreport')} className="report-link">Sales Report</button>
         <button onClick={() => goToReport('/packageReport2')} className="report-link">Package Report</button>
-        <button onClick={() => goToReport('/employeesanddepartments')} className="report-link">Employees and Depratments</button>
-        <button onClick={() => goToReport('/vehiclesandemployees')} className="report-link">Vehicles and Employees</button>
-        <button onClick={() => goToReport('/packagesender')} className="report-link">Package Sender Report</button>
-        <button onClick={() => goToReport('/totalamountuser')} className="report-link">Report on total amount user spent </button>
-        <button onClick={() => goToReport('/packagereport')} className="report-link">Report on packages </button>
-        <button onClick={() => goToReport('/reportE&P')} className="report-link">Report on employee's handling packages </button>
+        <button onClick={() => goToReport('/monthlysignups')} className="report-link">User Sign-Up Report</button>
+        <button onClick={() => goToReport('/employeesanddepartments')} className="report-link2">Employees and Depratments</button>
+        <button onClick={() => goToReport('/vehiclesandemployees')} className="report-link2">Vehicles and Employees</button>
+        <button onClick={() => goToReport('/packagesender')} className="report-link2">Package Sender Report</button>
+        <button onClick={() => goToReport('/totalamountuser')} className="report-link2">Report on total amount user spent </button>
+        <button onClick={() => goToReport('/packagereport')} className="report-link2">Report on packages </button>
+        <button onClick={() => goToReport('/reportE&P')} className="report-link2">Report on employee's handling packages </button>
 
       </div>
       <p className="report-description">
