aa6db79cc7eca669e5acfb53daee4990fb41b28f
Mustafa Sahin <110689298+mustafaemresahin@users.noreply.github.com>
2024-04-19 02:08:34 -0500

    about page links

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../css/About.css';
 import image from '../img/about-us-bg.png';
@@ -7,6 +7,15 @@ import '../css/register.css';
 import '../css/tracking.css'; 
 function About() {
   const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const id = localStorage.getItem('id');
+    setIsLoggedIn(!!token);
+    
+  }, []);
+
   return (
     <div className="about-us-container">
       <div className='about-us'>
@@ -25,7 +34,8 @@ function About() {
             <div className='button-holder'>
             <button onClick={()=> {navigate("/Send%20Package")}}>Send Package</button>
             <button onClick={()=>{window.location.href="/track"}}>Track Package</button>
-            <button onClick={()=>{window.location.href="/register"}}>Register</button>
+            {!isLoggedIn && <button onClick={()=>{window.location.href="/register"}}>Register</button>}
+            {isLoggedIn && <button onClick={()=> {navigate("/shop")}}>Post Office Shop</button>}
           </div>
 
 
@@ -48,7 +58,8 @@ function About() {
             <div className='service-info'>
               <h2>Our mission is to provide easy access to postal information and services, whether you're sending a love letter, an important document, or a care package to someone special. We understand the importance of reliable postal services in keeping the world connected, and our database is meticulously maintained by a dedicated team to ensure accuracy and relevance.</h2> 
             </div>
-             <button onClick={()=>{window.location.href="/register"}}>Get Started</button>   
+             {!isLoggedIn && <button onClick={()=>{window.location.href="/register"}}>Get Started</button>}
+             {isLoggedIn && <button onClick={()=>{window.location.href="/profile"}}>Profile</button>}
             </div>
 
 
@@ -67,7 +78,7 @@ function About() {
           <div className='service-info'>
             <h2>Discover a vast array of postal supplies at our post office, designed to meet all your mailing and shipping needs with ease and efficiency. From a wide selection of boxes and envelopes of various sizes to secure your items, to an extensive range of packaging materials ensuring their safe transit, we've got you covered.</h2> 
           </div>
-          <button onClick={()=>{window.location.href="/shop"}}>Get Supplies</button>  
+          <button onClick={()=> {navigate("/shop")}}>Get Supplies</button>
         </div>
       </div>
 
